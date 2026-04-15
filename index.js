90e9edf817a52ecbdbd1019ad16a84e25fcff077
sheenawangumo <sheenawangumo.contact@gmail.com>
2026-04-15 16:51:28 +0300

    added function

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+function createLoginTracker(userInfo){ 
+ let attemptCount = 0;
+    const loginHandler = (passwordAttempt) => {
+        if (attemptCount >= 3) {
+        return "Account is locked due to too many failed login attempts"; 
+         }
+        if (passwordAttempt === userInfo.password) { 
+            attemptCount = 0;
+        return "Login successful"; }
+          else  { 
+       attemptCount++; 
+      if (attemptCount >= 3) { 
+           return "Account is locked due to too many failed login attempts";
+          }
+        return "Login failed";
+    }
+  };
+    return loginHandler;
+  }
 
 
+ 
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
